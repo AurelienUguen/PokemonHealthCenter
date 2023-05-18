ac8b532c6bee3f89aa3c15939cefad132da638de
DorianRONDI <dorianrondi@gmail.com>
2023-05-18 20:53:50 +0200

    Bouton pour lister les Pokemons bléssés OK.Bouton pour soigner les Pokemons OK

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,54 @@
 import { Machine } from "./machine.js";
 import { Pokemon } from "./pokemon.js";
 
-const machine = new Machine (6);
+// Instance de la Machine a soigné les Pokeymaunes //
+const machine = new Machine(6);
 
-let pokedex : {[key: string]: Pokemon} = {
-    pikachu : new Pokemon('Pikachu', 'Pikaaaachhuuuu', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png', 150),
-    salameche : new Pokemon('Salameche', 'salaaameche', 'https://tinyurl.com/2w4dykhy', 200),
-    carapuce : new Pokemon('Carapuce', 'carapuuuce', 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png', 90),
-    evoli : new Pokemon('evoli', 'evoliii', 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/133.png', 110),
-    canartichau : new Pokemon('canartichau', 'canartichauuu', 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/083.png', 300),
-    psykokwak : new Pokemon('psykokwak', 'psykokwaaak', 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/054.png', 100),
+// List de nos instances de Pokeymaunes //
+let pokedex: { [key: string]: Pokemon } = {
+    pikachu: new Pokemon('Pikachu', 'Pikaaaachhuuuu', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png', 150),
+    salameche: new Pokemon('Salameche', 'salaaameche', 'https://tinyurl.com/2w4dykhy', 200),
+    carapuce: new Pokemon('Carapuce', 'carapuuuce', 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png', 90),
+    evoli: new Pokemon('evoli', 'evoliii', 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/133.png', 110),
+    canartichau: new Pokemon('canartichau', 'canartichauuu', 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/083.png', 300),
+    psykokwak: new Pokemon('psykokwak', 'psykokwaaak', 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/054.png', 100),
     ptitard: new Pokemon('ptitard', 'ptitaaaard', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/060.png', 100),
 };
-
-const pokeKeys = Object.keys(pokedex);
-
-console.log("pokeKeys = "+pokeKeys);
 
 pokedex.pikachu.wound(200);
 pokedex.canartichau.wound(10);
 
+// Variable qui accumule les Pokeymaunes bléssés ou hors jeux //
+let woundedPokemon: string[] = [];
 
-
-let woundedPokemon  = [];
-
-function isWounded(){
-
-    Object.keys(pokedex).forEach(function(pokemon) {
-        if(pokedex[pokemon].pv < pokedex[pokemon].maxPv){
-            console.log(pokemon);
+// Fonction de remplissage de la liste de Pokeymaunes bléssés ou hors jeux //
+function isWounded() {
+    Object.keys(pokedex).forEach(function (pokemon) {
+        if (pokedex[pokemon].pv < pokedex[pokemon].maxPv) {
+            woundedPokemon = woundedPokemon.concat(pokemon);
         }
     });
+    console.log(woundedPokemon);
+}
+
+function heal(list: string[]){
+    for(let i = 0; i < list.length; i++){
+        console.log(pokedex[list[i]].name+" à "+pokedex[list[i]].pv+" Pts de vie sur "+pokedex[list[i]].maxPv+" avant les soins");
+        pokedex[list[i]].pv = pokedex[list[i]].maxPv;
+        console.log(pokedex[list[i]].name+" à "+pokedex[list[i]].pv+" Pts de vie sur "+pokedex[list[i]].maxPv+" après les soins");
+    }
 
 }
-isWounded();
-
-
-//console.log(woundedPokemon);
-
-/*
-// machine.regen(pokedex[pokeKeys[i]]);
 
 const lineElt = document.querySelector('.line');
 
-function createCard(title: string, imageUrl: string) {
+/*
+* title = pokedex[woundedPokemon[?]].name
+* imageUrl = pokedex[woundedPokemon[?]].image
+* scream = pokedex[woundedPokemon[?]].scream
+* maxPv = pokedex[woundedPokemon[?]].maxPv
+*/
+function createCard(title: string, imageUrl: string, scream: string, maxPv: number) {
 
     const card = document.createElement("div");
     card.classList.add("full-card");
@@ -70,15 +75,24 @@ function createCard(title: string, imageUrl: string) {
     console.log('Hi!')
 }
 
+createCard('Carapuce', 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png', 'ouch!!', 150);
 
-createCard('Carapuce', 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png');
 
-const btnElmt = document.querySelector('.btn-wounded');
-const woundedKeys = Object.keys(woundedPokemon);
-console.log(woundedKeys)
-btnElmt?.addEventListener('click', () => {
-    
-    
+
+// les fonctions des boutons //
+
+const btnWoundedElmt = document.querySelector('.btn-wounded');
+const btnHealedElmt = document.querySelector('.btn-healed');
+
+btnWoundedElmt?.addEventListener('click', () => {
+    console.log('Wound');
+    isWounded();
+    console.log(pokedex[woundedPokemon[0]]);
 })
 
-*/
+btnHealedElmt?.addEventListener('click', () => {
+    console.log('heal');
+    heal(woundedPokemon);
+    woundedPokemon = [];
+    console.log(woundedPokemon);
+})
