ce1969fa3b4577d8d4f75a4b7dfd23b48fce7eda
DorianRONDI <dorianrondi@gmail.com>
2023-05-18 15:27:46 +0200

    pokeKeys de l'enfer

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,35 @@ import { Pokemon } from "./pokemon.js";
 
 
 const machine = new Machine (6);
-const pikachu = new Pokemon('Pikachu', 'Pikaaaachhuuuu', 'joliPik', 100);
 
+let pokedex = {
+    pikachu : new Pokemon('Pikachu', 'Pikaaaachhuuuu', 'joliPik', 150),
+    salameche : new Pokemon('salameche', 'salaaameche', 'joliPik', 200),
+    carapuce : new Pokemon('carapuce', 'carapuuuce', 'joliPik', 90),
+    evoli : new Pokemon('evoli', 'evoliii', 'joliPik', 110),
+    canartichau : new Pokemon('canartichau', 'canartichauuu', 'joliPik', 300),
+    psykokwak : new Pokemon('psykokwak', 'psykokwaaak', 'joliPik', 100),
+    ptitard: new Pokemon('ptitard', 'ptitaaaard', 'joliPik', 100),
+};
+
+const pokeKeys = Object.keys(pokedex);
+
+console.log(pokeKeys);
+pokedex.pikachu.wound(150);
+
+//type Pokemons = keyof pokedex;
+
+/*
+for (let i = 0; i < pokeKeys.length;i++){
+    const pokemon = pokedex[pokeKeys[i]];
+    console.log(pokemon);
+    if(pokedex[pokeKeys[i]].pv < pokedex[pokeKeys[i]].maxPv){
+        console.log("coucou");
+        console.log(pokedex[pokeKeys[i]].pv);
+        machine.regen(pokedex[pokeKeys[i]]);
+    }
+}
+*/
 //pikachu.pv = 10;
-console.log(pikachu.wound(50));
-machine.regen(pikachu);
-console.log(pikachu);
-
+//machine.regen(pokedex.[pokeKeys[i]]);
+console.log(pokedex);
