25f8d00a55bfa945c9066f4fa64dd041b0039fe8
Marine PAYET <marine.payet3@gmail.com>
2023-05-18 12:30:14 +0200

    créa des classes

---
+++ src/pokemon.ts
@@ -0,0 +1,55 @@
+
+export class Pokemon  {
+    
+    private _pv: number;
+
+    constructor(private _name: string, private _scream: string, private _image: string, private _maxPv :number){
+        this._pv = _maxPv;
+        this._maxPv = _maxPv;
+    }
+    
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
+    
+    public get scream(): string {
+        return this._scream;
+    }
+    public set scream(value: string) {
+        this._scream = value;
+    }
+    
+    public get pv(): number {
+        return this._pv;
+    }
+
+    public set pv(value: number) {
+        this._pv;
+    }
+    
+    public get image(): string {
+        return this._image;
+    }
+
+    public set image(value: string) {
+        this._image = value;
+    }
+
+    public get maxPv():number {
+        return this._maxPv;
+    }
+
+    regen = () => {
+        this.pv = this.maxPv;
+        return this.scream;
+    }    
+
+    wound = (attackPv :number)=> {
+        this.pv -= attackPv;
+        return ('Ouch')
+    }
+}
+
