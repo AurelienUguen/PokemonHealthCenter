4eb77bf77230ac2089ead38dc723c09884c7eb74
Marine PAYET <marine.payet3@gmail.com>
2023-05-22 13:40:27 +0200

    ok cris

--- src/app.ts
+++ src/app.ts
@@ -48,15 +48,86 @@ let pokedex: { [key: string]: Pokemon } = {
         "https://assets.pokemon.com/assets/cms2/img/pokedex/full/060.png",
         100
     ),
+    bulbizarre: new Pokemon(
+        "Bulbizarre",
+        "Bulbiiii",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+        105
+    ),
+    melofee: new Pokemon(
+        "Mélofée",
+        "Mélooooofée",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/035.png",
+        150
+    ),
+    goupix: new Pokemon(
+        "Goupix",
+        "Goupiiiix",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/037.png",
+        200
+    ),
+    caninos: new Pokemon(
+        "Caninos",
+        "Caniiiinos",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/058.png",
+        100
+    ),
+    racaillou: new Pokemon(
+        "Racaillou",
+        "Racaillouuuu",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/074.png",
+        100
+    ),
+    ponyta: new Pokemon(
+        "Ponyta",
+        "Popoponyta",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/077.png",
+        190
+    ),
+    otaria: new Pokemon(
+        "Otaria",
+        "Otariiia",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/086.png",
+        140
+    ),
+    tadmorv: new Pokemon(
+        "Tadmorv",
+        "Tadmoooooorv",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/088.png",
+        260
+    ),
+    onix: new Pokemon(
+        "Onix",
+        "Oniiiiix",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/095.png",
+        100
+    ),
+    lipoutou: new Pokemon(
+        "Lipoutou",
+        "Lipoutoupoutou",
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/full/124.png",
+        100
+    )
 }
-
+​
 pokedex.pikachu.wound(200);
-pokedex.canartichau.wound(140);
-pokedex.psykokwak.wound(70);
-pokedex.carapuce.wound(74);
-pokedex.ptitard.wound(88);
-pokedex.evoli.wound(25);
+pokedex.canartichau.wound(10);
+pokedex.psykokwak.wound(23);
 pokedex.salameche.wound(133);
+pokedex.carapuce.wound(74);
+pokedex.evoli.wound(17);
+pokedex.ptitard.wound(34);
+​
+pokedex.bulbizarre.wound(34);
+pokedex.melofee.wound(37);
+pokedex.goupix.wound(74);
+pokedex.caninos.wound(64);
+pokedex.racaillou.wound(34);
+pokedex.ponyta.wound(60);
+pokedex.otaria.wound(14);
+pokedex.tadmorv.wound(3);
+pokedex.onix.wound(100);
+pokedex.lipoutou.wound(355);
 
 
 
@@ -130,6 +201,15 @@ function createPokemonCard(
     pokemon._healthContainer.classList.add("health");
     cardHealthContainer.appendChild(pokemon._healthContainer);
     pokemon._healthContainer.style.width = calcHealthPerc(pokemon.pv, pokemon.maxPv)
+    
+    const cardScreamContainer = document.createElement("div");
+    cardScreamContainer.classList.add("screamCont");
+    cardBody.appendChild(cardScreamContainer);
+
+    pokemon._screamContainer = document.createElement("div");
+    pokemon._screamContainer.classList.add("scream");
+    cardScreamContainer.appendChild(pokemon._screamContainer);
+    // pokemon._screamContainer.innerHTML= pokemon.scream;
 
     let actualLifeInNumber = Number(pokemon._healthContainer.style.width.replace(/[^0-9]/g, ''));
 
@@ -167,7 +247,7 @@ let button: boolean;
 const music = new Audio('./assets/PokeCenter2.mp3');
 
 btnMachineElmt.addEventListener("click", () => {
-
+    
     if (button) {
         heal(listWounded);
         woundedPokemon = [];
@@ -181,7 +261,19 @@ btnMachineElmt.addEventListener("click", () => {
         };
         btnMachineElmt.setAttribute('disabled', '');
         setTimeout(() => {btnMachineElmt.textContent = "Who is wounded ?"}, 3000);
+        
         setTimeout(() => {cards.innerHTML = ""}, 3000);
+        
+
+        for (let i = 0; i < listWounded.length; i++) {
+            const pokeScream = listWounded[i];
+            setTimeout(() => {
+                if (pokeScream._screamContainer) {
+                    pokeScream._screamContainer.innerHTML = `${pokeScream.scream}`;
+                }
+            }, 2000);
+        }
+
         setTimeout(() => {btnMachineElmt.removeAttribute('disabled')}, 3000);
         music.play();
         button = false;
